7ef28523a8faa641d034f689fb691211bdb1364d
leonelpaulino <leonel.paulino18@gmail.com>
2014-11-30 21:37:58 -0400

    Clase user.
    Agregue las funciones de getpostVersion, getProjects, newUser

--- app/models/User.php
+++ app/models/User.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
-	@brief: Clase que representa un usuario de la base de datos.
-
+	@brief  Clase que representa un usuario de la base de datos.
+	@autor  Leonel Paulino
 */
 
 class User extends Eloquent {
@@ -23,8 +23,30 @@ class User extends Eloquent {
 	/*
 		@brief: Retorna todos los proyectos al que pertenece este usuario.
 		@return Projects  
+		@autor Leonel Paulino
 	*/
 	public function getProjects(){
 		return $this->belongsToMany('project', 'project_user_role', 'user_id', 'project_id');
+	}
+	/*
+		@brief: Crea un nuevo usuario en la base de datos y retorna el usuario creado. 
+		@param nombre		nombre del usuario que se va crear
+		@param lastname 	apellido del usuario que se va crear.
+		@param username 	nombre de usuario.
+		@param intialpass   contraseña del usuario.
+		@param defaultid 	El id por default del usuario.
+		@param email 		email del usuario.
+		@return User 		
+		@autor Leonel Paulino
+	*/
+	public static function newUser($nombre,$lastname,$username,$intialpass,$defaultid,$email){
+		$this->nombre = $nombre;
+		$this->lastname = $lastname;
+		$this->username = $username;
+		$this->password = $intialpass;
+		$this->default_role_id = $defaultid;
+		$this->email = $email;
+		$this->save();
+		return $this;
 	}
 }
