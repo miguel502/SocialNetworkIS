946002da5660ecd4c499a6289531e85beb20e736
Michael M. Rosario <mchl0208@gmail.com>
2014-12-04 02:40:19 -0400

    test

--- app/tests/StageTest.php
+++ app/tests/StageTest.php
@@ -5,14 +5,15 @@ class StageTest extends TestCase {
     public function testnewStage()
     {
     	$stage = true;
-    	$this->assertTrue($stage == true);
+    	$this->assertTrue(true);
     }
 
     public function testgetStage()
     {
-    	$stageid = 1;
-    	$stage = Stage::getStage(1,1);
-    	$this->assertCount(0,0);
+        $position_id = 1;
+        $project_id = 5;
+        $stage = Stage::getStage($position_id, $project_id);
+    	$this->assertCount(1, $stage);
     }
 
 }
